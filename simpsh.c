093bf738ed45cf3b1fde37f81e6e25d18b1f5352
Sarah Le Cam <sdl83@cornell.edu>
2017-02-06 01:24:56 -0500

    hopefully fix

--- simpsh.c
+++ simpsh.c
@@ -241,34 +241,24 @@ void option_profile(int who) {
 		// success
 		struct timeval user_time = usage.ru_utime;
 		struct timeval os_time = usage.ru_stime;
-		double user;
-		double system;
-		double total;
 
 		if (who == RUSAGE_SELF) {
-
-			user = (double)((double)user_time.tv_sec + (double)user_time.tv_usec/1000000) -
-				(double)((double)old_user_time.tv_sec + (double)old_user_time.tv_usec/1000000);
-			system = (double)((double)os_time.tv_sec + (double)os_time.tv_usec/1000000) -
-				(double)((double)old_system_time.tv_sec + (double)old_system_time.tv_usec/1000000);
-			total = user + system;
-
 			printf("\nRessources used by option: \n");
-			printf("Time spent executing user instructions: %f seconds; \n", user);
-			printf("Time spent on operating system code on behalf of process: %f seconds; \n", system);
-			printf("Total CPU time used by process: %f seconds; \n", total);
+			printf("Time spent executing user instructions: %u seconds, %u microseconds; \n",
+				(int)((double)user_time.tv_sec - (double)old_user_time.tv_sec),
+				(int)((double)user_time.tv_usec - (double)old_user_time.tv_usec));
+			printf("Time spent on operating system code on behalf of process: %u seconds, %u microseconds; \n",
+				(int)((double)os_time.tv_sec - (double)old_system_time.tv_sec),
+				(int)((double)os_time.tv_usec - (double)old_system_time.tv_usec));
 			printf("\n");
 		} else {
-			user = (double)((double)user_time.tv_sec + (double)user_time.tv_usec/1000000) -
-				(double)((double)old_child_user_time.tv_sec + (double)old_child_user_time.tv_usec/1000000);
-			system = (double)((double)os_time.tv_sec + (double)os_time.tv_usec/1000000) -
-				(double)((double)old_child_system_time.tv_sec + (double)old_child_system_time.tv_usec/1000000);
-			total = user + system;
-
 			printf("\nRessources used by children processes: \n");
-			printf("Time spent executing user instructions: %f seconds; \n", user);
-			printf("Time spent on operating system code on behalf of process: %f seconds; \n", system);
-			printf("Total CPU time used by process: %f seconds; \n", total);
+			printf("Time spent executing user instructions: %u seconds, %u microseconds; \n",
+				(int)((double)user_time.tv_sec - (double)old_child_user_time.tv_sec),
+				(int)((double)user_time.tv_usec - (double)old_child_user_time.tv_usec));
+			printf("Time spent on operating system code on behalf of process: %u seconds, %u microseconds; \n",
+				(int)((double)os_time.tv_sec - (double)old_child_system_time.tv_sec),
+				(int)((double)os_time.tv_usec - (double)old_child_system_time.tv_usec));
 			printf("\n");
 		}
 
